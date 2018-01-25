6bca8e8fdd4e2b0de8e3cba2838fdc68895f76e9
Adam Stewart <adam@marchinglizard.net>
2018-01-25 16:05:36 -0600

    Add podcast page (#639)
    * Add podcast page.
    
    * Edits and remove audio file.

--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -6,7 +6,7 @@ $(function() {
     });
 
     // Add deep anchor links to projects page.
-    anchors.add('.projects .project-title, .post h2, .post h3, .post h4, .post h5');
+    anchors.add('.projects .project-title, .post h2, .post h3, .post h4, .post h5, .podcast-title');
 
     // Add Semantic UI related classes to tables.
     $('table').addClass('ui table');
